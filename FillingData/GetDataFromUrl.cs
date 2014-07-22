67b21df627b001dcbcead1450c36fe033cadc1ca
Albert Davletov <albert.davletov@mail.ru>
2014-07-23 00:23:00 +0400

    fix Views + testing Json Convert

--- FillingData/GetDataFromUrl.cs
+++ FillingData/GetDataFromUrl.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.FillingData
+﻿using Newtonsoft.Json.Linq;
+
+namespace Testing.FillingData
 {
     using System.IO;
     using System.Net;
@@ -10,8 +12,8 @@
         {
             var result = string.Empty;
             var myRequest = (HttpWebRequest)WebRequest.Create(url);
-            myRequest.Proxy.Credentials = new NetworkCredential("DavletovA", "Tgg567876c", "RUSSIA");
-            myRequest.Proxy = WebRequest.DefaultWebProxy;
+            /*myRequest.Proxy.Credentials = new NetworkCredential("DavletovA", "Tgg567876c", "RUSSIA");
+            myRequest.Proxy = WebRequest.DefaultWebProxy;*/
 
             var myResponse = (HttpWebResponse)myRequest.GetResponse();
             var responseStream = myResponse.GetResponseStream();
@@ -20,6 +22,10 @@
             {
                 using (var sr = new StreamReader(responseStream))
                 {
+                    /*var str = sr.ReadToEnd();
+                    JObject parsed = JObject.Parse(str);
+                    var tmp = parsed["elements"];*/
+
                     result = sr.ReadToEnd().Replace("{\"elements\":", "").Replace(",\"linked\":{}}", "");
                 }
             }
@@ -30,17 +36,24 @@
         {
             var result = string.Empty;
             var myRequest = (HttpWebRequest)WebRequest.Create(url);
-            myRequest.Proxy.Credentials = new NetworkCredential("DavletovA", "Tgg567876c", "RUSSIA");
-            myRequest.Proxy = WebRequest.DefaultWebProxy;
+            /*myRequest.Proxy.Credentials = new NetworkCredential("DavletovA", "Tgg567876c", "RUSSIA");
+            myRequest.Proxy = WebRequest.DefaultWebProxy;*/
 
             var myResponse = (HttpWebResponse)myRequest.GetResponse();
             var responseStream = myResponse.GetResponseStream();
+
+           
 
             if (responseStream != null)
             {
                 using (var sr = new StreamReader(responseStream))
                 {
                     result = sr.ReadToEnd();
+
+                    /*JObject parsed = JObject.Parse(result);
+                    parsed["elements"]*/
+
+
                     var ind = result.IndexOf(",\"linked\":{");
                     result = result.Substring(0, ind);
 
