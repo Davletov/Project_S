9641c7d510888dedf92256e45f8e9cebc195b65f
Evgeny <deviant.hell@mail.ru>
2014-09-01 15:18:59 +0400

    youtube

--- Web/Controllers/YoutubeController.cs
+++ Web/Controllers/YoutubeController.cs
@@ -10,6 +10,10 @@ using Google.Apis.YouTube.v3.Data;
 using Google.GData.Client;
 using Google.GData.YouTube;
 using Google.YouTube;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Web.Models;
+using Web.Models.Criteria;
 using Web.Models.YouTube;
 using Video = Google.YouTube.Video;
 using YouTubeService = Google.Apis.YouTube.v3.YouTubeService;
@@ -25,10 +29,11 @@ namespace Web.Controllers
 
         public const string CRITERIA_SAMPLE = "linear mathematics lecture";
 
+        private UserManager<Profile> userManager = new UserManager<Profile>(new UserStore<Profile>(new BdContext()));
         private YouTubeRequestSettings requestSettings = new YouTubeRequestSettings(APPLICATION_NAME, DEVELOPER_KEY);
 
         public ActionResult Index(int pageNumber = 0)
-        {
+        {            
             YouTubeRequest request = new YouTubeRequest(requestSettings);
             YouTubeQuery q = new YouTubeQuery(YouTubeQuery.DefaultVideoUri);
             q.Query = CRITERIA_SAMPLE;
@@ -49,24 +54,34 @@ namespace Web.Controllers
             return View(res);
         }
 
-        public async Task<ActionResult> Index2(string pageToken)
+        public async Task<ActionResult> Index2(string criteriaName, string pageToken)
         {
+            Profile user = userManager.FindById(User.Identity.GetUserId());
+            ViewBag.SecondLevelCriterias = user.SecondLevelCriteria.Select(x => x.Name).ToList();
+
+            if (string.IsNullOrEmpty(criteriaName))
+            {
+                return View(new List<YoutubeMaterial>());
+            }
+
             YouTubeService youtubeService = new YouTubeService(new BaseClientService.Initializer()
             {
                 ApplicationName = APPLICATION_NAME,
                 ApiKey = "AIzaSyD5-fxmngznhYSJgEPn-uAmkaaSisRvMr4"
             });
 
+            List<YoutubeMaterial> materials = new List<YoutubeMaterial>();
+            string query = string.Join(",",
+                user.SecondLevelCriteria.Where(x => x.Name == criteriaName).Select(x => x.Name));
+
+
             SearchResource.ListRequest searchListRequest = youtubeService.Search.List("snippet");
-            searchListRequest.Q = CRITERIA_SAMPLE;
-            //searchListRequest.Order = SearchResource.ListRequest.OrderEnum.Relevance;
-            //searchListRequest.VideoDuration = SearchResource.ListRequest.VideoDurationEnum.Long;
-            searchListRequest.MaxResults = 50;
+            searchListRequest.Q = query;
+            searchListRequest.MaxResults = 10;
             searchListRequest.PageToken = pageToken;
             
-            SearchListResponse searchListResponse = await searchListRequest.ExecuteAsync();           
+            SearchListResponse searchListResponse = await searchListRequest.ExecuteAsync();
 
-            List<YoutubeMaterial> materials = new List<YoutubeMaterial>();
             foreach (SearchResult searchResult in searchListResponse.Items)
             {
                 switch (searchResult.Id.Kind)
@@ -81,16 +96,17 @@ namespace Web.Controllers
                         break;
 
                     case "youtube#channel":
-                        
+
                         break;
 
                     case "youtube#playlist":
-                        
+
                         break;
                 }
             }
             ViewBag.NextPageToken = searchListResponse.NextPageToken;
             ViewBag.PrevPageToken = searchListResponse.PrevPageToken;
+            ViewBag.CriteriaName = criteriaName;
             return View(materials);
         }
     }
