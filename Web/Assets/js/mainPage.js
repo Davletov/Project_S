fd7f5aa8f51cac9cd9b17adda2d075885f085f41
Albert Davletov <albert.davletov@mail.ru>
2015-01-24 17:51:47 +0400

    working by main page

---
+++ Web/Assets/js/mainPage.js
@@ -0,0 +1,41 @@
+﻿/*********************************************************************/
+var player;
+window.startYoutube = function () {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+function onPlayerStateChange(event) {
+}
+function stopVideo() {
+    player.stopVideo();
+}
+/*********************************************************************/
+$('.thumbnail').click(function () {
+    var mask = $('<a/>', { href: 'javascript:{}' });
+    mask.addClass('mask');
+    $('body').append(mask);
+    var player = $('<div/>', { id: 'player' });
+    $('body').append(player);
+
+    window.startYoutube();
+
+    function closeWindow() {
+        if (confirm("Close?")) {
+            this.remove();
+            $('#player').remove();
+        }
+    }
+
+    mask.click(closeWindow);
+});
